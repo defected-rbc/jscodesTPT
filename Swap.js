7eea72deaa61b461ccc0f33cdb78293be4ef6144
Gunjan Aggarwal <gunjanagg2004@gmail.com>
2024-07-25 22:59:22 +0530

    assignment 1

--- Swap.js
+++ Swap.js
@@ -1,9 +1,9 @@
 let a = 7;
 let b = 4;
 console.log("The value befor a = "+ a + " b = " +b );
-let c = a;
-a = b;
-b = c;
+a = a + b;
+b = a - b;
+a = a - b;
 console.log("The value after  a = "+ a + " b = " +b );
 
 // The value befor a = 7 b = 4
